8ab1480f52f217ca9fdae03e4057068855257e87
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-07-08 08:05:50 +0000

    Generating YAML config wip
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- csv2yaml.rb
+++ csv2yaml.rb
@@ -15,19 +15,36 @@ def convert(fn)
     map[n] = [] unless map.key?(n)
     map[n] << dn
   end
+  unknowns = {}
   map.each do |n, names|
     names = names.sort.uniq
     re = '^[[:space:]]*'
     l = names.length
     re += '(' if l > 1
     names.each_with_index do |name, i|
+      name.each_char do |r|
+          if (r >= 'a' && r <= 'z') || (r >= '0' && r <= '9') || r.ord > 0x80
+          re += r
+        elsif r >= 'A' && r <= 'Z'
+          re += r.downcase
+        elsif ['!', '/', ',', '-', '&', ']', '~', "'", '"'].include?(r)
+          re += r + '?'
+        elsif [' ', "\t"].include?(r)
+          re += '[[:space:]]*'
+        elsif ['|', '(', '.', '*', '['].include?(r)
+          re += '\\' + r + '?'
+        else
+          unknowns[r] = 0 unless unknowns.key?(r)
+          unknowns[r] += 1
+        end
+      end
       re += '|' if i < l-1
     end
     re += ')' if l > 1
     re += '[[:space:]]*$'
-    puts re
-    binding.pry if l > 2
+    puts "  ['" + re + "'], '" + n + "']"
   end
+  binding.pry if unknowns.length > 0
 end
 
 if ARGV.size < 1
